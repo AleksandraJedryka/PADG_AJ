1a055ff1f992442cdabb566209ac8e58a1b870ba
Ola <u56125@student.wat.edu.pl>
2025-12-01 14:58:40 +0100

    feat:frames that adapt to window size

--- folder/model.py
+++ folder/model.py
@@ -1,6 +1,4 @@
 users:list=[
     # {"name":'Kasia','location':'Warszawa','posts':3,"img_url":""},
-    # {"name":'Karolina','location':'Kraków','posts':5,"img_url":""},
-    # {"name":'Sylwia','location':'Wrocław','posts':2,"img_url":""},
 ]
 
